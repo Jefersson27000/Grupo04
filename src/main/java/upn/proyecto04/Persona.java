f5a479d1d6cb5fcef35319411a3bc97f102139ff
jefersson <jeferssonmezaespinoza27@hotmail.com>
2025-11-23 11:00:27 -0500

    Carga inicial

---
+++ src/main/java/upn/proyecto04/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upn.proyecto04;
+
+/**
+ *
+ * @author Admin
+ */
+public class Persona {
+       
+       private String nombre;
+       private String apellido;
+       private int edad;
+    
+}
