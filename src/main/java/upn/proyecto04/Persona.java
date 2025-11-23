808d10eccbe5c03d789c54557b7d648b377704cf
jefersson <jeferssonmezaespinoza27@hotmail.com>
2025-11-23 12:59:53 -0500

    agregar método mostrarDatos en clase Persona

--- src/main/java/upn/proyecto04/Persona.java
+++ src/main/java/upn/proyecto04/Persona.java
@@ -15,5 +15,67 @@ public class Persona {
        private int edad;
        private String telefono;
        private String correo;
+
+    public Persona(String nombre, String apellido, int edad, String telefono, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.correo = correo;
+        
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", telefono=" + telefono + ", correo=" + correo + '}';
+    }
+    
+    public void mostrarDatos() {
+    System.out.println("Nombre: " + nombre);
+}
+
     
 }
